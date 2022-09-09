f3002db7026edc4f775163f74c3fc2193d21a86d
Juliette Pretot <julsh@google.com>
2022-09-09 10:29:55 +0000

    list targets

--- tests/RustTest.sh
+++ tests/RustTest.sh
@@ -37,7 +37,8 @@ check_test_result "Rust serde tests"
 
 cd ../rust_test_no_std_compilation
 readonly INSTALLED_TARGETS="$(rustup target list)"
-if [[ ! "$INSTALLED_TARGETS" == *"x86_64-unknown-none (installed)"* ]]; then
+readonly TEST_NO_STD_TARGET="wasm32-unknown-unknown"
+if [[ ! "$INSTALLED_TARGETS" == *"${TEST_NO_STD_TARGET} (installed)"* ]]; then
   rustup target add x86_64-unknown-none
 fi
 cargo build
